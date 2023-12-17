16eebea841c249663cd654cd9c13754b2cd558e6
miRx923 <94078303+miRx923@users.noreply.github.com>
2023-12-17 20:13:11 +0100

    Added ploting of the field to field_utils.py

--- src/field_utils.py
+++ src/field_utils.py
@@ -30,3 +30,43 @@ def print_field_in_terminal(field):
     print()
     print('\n'.join([' '.join(['{:<4}'.format(str(item)) for item in row]) for row in field]))
     print()
+
+
+
+def print_field_using_plot(field):
+    """ Function for printing the final field with plot """
+
+    rows = len(field)
+    cols = len(field[0])
+
+    # Map characters and numbers to numerical values
+    # Create a new array of zeros with the same shape
+    mapped_array = np.zeros_like(field, dtype=float)  
+
+    # Constants for the special characters
+    NUMBER_VALUE = 1
+    E_VALUE = 2
+    S_VALUE = 3
+    STAR_VALUE = 4
+
+    for i, row in enumerate(field):
+        for j, item in enumerate(row):
+            if item == 'S':
+                mapped_array[i][j] = S_VALUE
+            elif item == 'E':
+                mapped_array[i][j] = E_VALUE
+            elif item == '*':
+                mapped_array[i][j] = STAR_VALUE
+            else:
+                mapped_array[i][j] = NUMBER_VALUE
+
+    # Create a custom color map
+    cmap = ListedColormap(['gray', 'red', 'green', 'blue'])  # gray for numbers, green for 'S', red for 'E', blue for '*'
+
+    # Plotting
+    plt.figure(figsize=(rows, cols))
+    plt.imshow(mapped_array, cmap=cmap)
+    plt.title('Floodfill plot visualisation')
+    plt.xticks(np.arange(mapped_array.shape[1]))
+    plt.yticks(np.arange(mapped_array.shape[0]))
+    plt.show()
